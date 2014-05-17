4ff477f0027f929722181c42be9dfeea89968ea8
bajunio <bajunio@gmail.com>
2014-05-17 09:10:23 -0700

    update to solo challenge - seems to be complete

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -12,13 +12,42 @@
 # What are the steps needed to solve the problem?
 
 
+
 # 2. Initial Solution
+=begin
+def separate_comma(value)
+	return value if value < 1000
+	value = value.to_s
+	puts "this is my value: #{value}"
+	array = []
+	array = value.split("").to_a.reverse!
+	(3..array.length + 1).step(4).each {|index| array.insert(index, ",")}
+	puts "this is my array: #{array}"
+	array = array.join()
+	puts "this is my joined array: #{array}"
+	array = array.reverse!
+	puts "this is my joined reversed array: #{array}"
+end
 
 
-
+num = Random.new.rand(100_000_000...100_000_000_000)
+puts num
+separate_comma(num)
+=end
 
 # 3. Refactored Solution
 
 
+def separate_comma(value)
+	value.to_s if value < 1000
+	array = Array.new(value.to_s.each_char.to_a.reverse!)
+	(3...array.length).step(4).each {|index| array.insert(index, ",")}
+	array = array.join().reverse!
+end
 
 # 4. Reflection 
+=begin
+
+
+
+=end
