68aaf20da6f4f655ba345582c21a1e5456f30485
bajunio <bajunio@gmail.com>
2014-05-19 15:36:18 -0700

    adding initial solution for challenge 1

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -1,32 +1,35 @@
 # U2.W5: Die Class 1: Numeric
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input:  Number of sides for our new die.
+# Output:  Ability to question amount of sides and also roll random.
 # Steps:
+
+
 
 
 # 3. Initial Solution
 
 class Die
   def initialize(sides)
-    # code goes here
+    raise ArgumentError "Die must contain at least one side." if sides < 1
+    @sides = sides
   end
   
   def sides
-    # code goes here
+    self.@sides
   end
   
   def roll
-    # code goes here
+    1 + rand(@sides)
   end
 end
 
-
+die = Die.new(6)
 
 # 4. Refactored Solution
 
@@ -37,6 +40,8 @@ end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+p die.new(6).sides == 6
+p die(6).roll > 1 < 6
 
 
 
