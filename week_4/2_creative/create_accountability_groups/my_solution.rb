cf93303aef8c0a914929e5b7cda27899734867d9
bajunio <bajunio@gmail.com>
2014-05-18 20:53:11 -0700

    final touches to 2

--- week_4/2_creative/create_accountability_groups/my_solution.rb
+++ week_4/2_creative/create_accountability_groups/my_solution.rb
@@ -1,15 +1,19 @@
+=begin
 # U2.W4: Create Accountability Groups
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
+Define method create_groups accepting one argument
+	New Array created by shuffling argument, sliceing into four, and putting to an array
+End
 
-# Input:  Accept hash table with names and email addresses.
-# Output:  Sorts hash table and outputs even groups of no less than 4.
+# Input:  Accept array with names.
+# Output:  Shuffles array and outputs even groups of no less than 4 (when possible).
 # Steps:  Start Sublime, ???, profit.  
 
-
+=end
 # 3. Initial Solution
 
 #creating hash of users... joy!
@@ -35,8 +39,7 @@ dbc_boots_array = [
 	"Lauren Kroner", "Albert Alquisola", "Ernie Salazar", "Jared Chevalier", "William Pangestu"
 ]
 
-#magic time!
-
+=begin
 
 def create_groups(array)
 	array = array.shuffle 	
@@ -44,8 +47,21 @@ def create_groups(array)
 	$groups = array.each_slice(4).to_a
 end
 
+=end
+
 # 4. Refactored Solution
+
+def create_groups(array)
+	Array.new(array.shuffle.each_slice(4).to_a)
+end
+
+
 =begin
+Update, I totally lied.  The new refactor up there is WAAAY better than my nonsense from before.  
+I'm not sure why I was worried about declaring all of those variables in the method.  Cleaned them
+up and was able to knock it all our in one simple Array.new() command.  I LOVE CHAINING METHODS!!
+
+
 This method was built using a lot of built-in methods for Ruby.
 The only real refactoring I did was add global scope for my $groups variable.
 I had ran into trouble attemping to print (I like the display of print over puts
@@ -60,11 +76,30 @@ I had ran into trouble attemping to print (I like the display of print over puts
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+p create_groups(dbc_boots_array).is_a?(Array)
+p create_groups(dbc_boots_array) != dbc_boots_array
 
 
-
-
-
+=begin
 # 5. Reflection 
 
+Are there built-in methods to do this in Ruby?
+Will you try to avoid putting the same people together more than once?
+If I run this program three times in a row, will the program give me three different outputs? Is that good or bad?
+Should it remember past outputs or not?
 
+Of course, there is a built in method to take over the world, I'm sure of it.  I'll find it soon enough.
+The superstar method here was certainly Array#each_slice.  I was such a nice find when I was trying to 
+brainstorm on splitting this array up into chunks.  Wild how simple it can be just levering the correct
+tool for the job!
+
+Every time this method is called on an array it will output a different configuration.  It is what it is,
+really.  Its fine if you were just needing it to output a quick seating chart.  If you really wanted
+to track you'd have to store these groups of 4 into a hash and have some sort of iteration check if
+the values of said hash include any of the same folks in the same group.  
+
+Once again, feels like for the sake of assigning groups for this, I'd say it is not really a big deal
+for it to house the past groupings to check against.  
+
+
+=end
