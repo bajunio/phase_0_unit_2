ed6522b16a8bdc78f605b685de3cc9a5478a6b70
bajunio <bajunio@gmail.com>
2014-05-26 15:57:23 -0700

    Challenge 2, fully complete

--- week_5/2_die/my_solution.rb
+++ week_5/2_die/my_solution.rb
@@ -9,6 +9,23 @@
 # Output:  Random display from one of the sides.
 # Steps:
 
+define Die class
+
+define initialize accepting one arg, labels
+  raise AE if labels empty?
+  set local var @labels equal to labels
+end
+
+define sides, no args
+  return @labels length
+end
+
+define roll, no args
+  call a random element from array via rand with cap of @labels.length
+end
+
+end class
+
 
 # 3. Initial Solution
 =begin
@@ -34,7 +51,7 @@ end
 
 class Die
   def initialize(labels)
-    raise ArgumentError, "Must contain at lease one label." if labels.empty?
+    raise ArgumentError, "Must contain at least one label." if labels.empty?
     @labels = labels
   end
 
@@ -47,17 +64,27 @@ class Die
   end
 end
 
-array = ["A", "B", "C", "D", "E", "F"]
-die = Die.new(array)
-p die.sides
-p die.roll
+#array = ["A", "B", "C", "D", "E", "F"]
+#die = Die.new(array)
+#p die.sides
+#p die.roll
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+p Die.instance_method(:initialize).arity == 1
+p Die.new(["A", "B", "C", "D", "E", "F"]).sides == 6
+p Die.new(["A", "B", "C", "D", "E", "F"]).roll.include?("J") == false
 
 
 
 
 
 # 5. Reflection 
+=begin 
+
+Another fairly straight-forward challenge.  I see that basically we were
+able to asign a letter to a particular side of the die we created.  Certain
+board games would benefit from this assoication. 
+
+=end
