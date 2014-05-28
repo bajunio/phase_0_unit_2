590ac2e194d10444cd0ebd8348dc7c5fb3223173
bajunio <bajunio@gmail.com>
2014-05-28 15:28:14 -0700

    added in some initial thoughts

--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -1,63 +1,57 @@
 # U2.W6: Drawer Debugger
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 
 # 2. Original Code
 
 class Drawer
-
 attr_reader :contents
-
 # Are there any more methods needed in this class?
+	def initialize
+		@contents = []
+		@open = true
+	end
 
-def initialize
-@contents = []
-@open = true
+	def open
+		@open = true
+	end
+
+	def close
+		@open = false
+	end 
+
+	def add_item
+		@contents << item
+	end
+
+	def remove_item(item = @contents.pop) #what is `#pop` doing?
+		@contents.delete(item)
+	end
+
+	def dump  # what should this method return?
+		puts "Your drawer is empty."
+	end
+
+	def view_contents
+		puts "The drawer contains:"
+		@contents.each {|silverware| puts "- " + silverware.type }
+	end
 end
-
-def open
-@open = true
-end
-
-def close
-@open = false
-end 
-
-def add_item
-@contents << item
-end
-
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-@contents.delete(item)
-end
-
-def dump  # what should this method return?
-puts "Your drawer is empty."
-end
-
-def view_contents
-puts "The drawer contains:"
-@contents.each {|silverware| puts "- " + silverware.type }
-end
-
 
 class Silverware
 attr_reader :type
-
 # Are there any more methods needed in this class?
+	def initialize(type, clean = true)
+		@type = type
+		@clean = clean
+	end
 
-def initialize(type, clean = true)
-@type = type
-@clean = clean
-end
-
-def eat
-puts "eating with the #{type}"
-@clean = false
-end
-
+	def eat
+		puts "eating with the #{type}"
+		@clean = false
+	end
 end
 
 knife1 = Silverware.new("knife")
@@ -67,7 +61,7 @@ silverware_drawer.add_item(knife1)
 silverware_drawer.add_item(Silverware.new("spoon"))
 silverware_drawer.add_item(Silverware.new("fork")) 
 silverware_drawer.view_contents
-
+=begin 
 silverware_drawer.remove_item
 silverware_drawer.view_contents
 sharp_knife = Silverware.new("sharp_knife")
@@ -100,3 +94,8 @@ fork.eat
 
 
 # 5. Reflection 
+
+
+
+
+=end
