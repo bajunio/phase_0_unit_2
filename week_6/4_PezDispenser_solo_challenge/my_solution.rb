fce8e4e759b67d149eb17540be0533dded915dbe
bajunio <bajunio@gmail.com>
2014-05-30 11:04:13 -0700

    Challenge 4, fully complete

--- week_6/4_PezDispenser_solo_challenge/my_solution.rb
+++ week_6/4_PezDispenser_solo_challenge/my_solution.rb
@@ -1,7 +1,7 @@
 # U2.W6: PezDispenser Class from User Stories
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Review the following user stories:
 # - As a pez user, I'd like to be able to "create" a new pez dispenser with a group of flavors that 
@@ -15,26 +15,63 @@
 
 
 # 2. Pseudocode
+=begin
+new class PezDispenser
+	define initialize accepting one arg, flavors_array
+		set instance var @flavors equal to flavors_array
+	end
 
+	define see_all_pez, no args
+		iterate through @flavors and display contents
+	end
 
+	define add_pez accepting one arg, flavor
+		push flavor to @flavors array 
+	end
 
+	define get_pez
+		pop the last element from @flavors
+	end
+
+	define pez_count
+		return @flavors size
+	end
+
+end class
+
+=end
 # 3. Initial Solution
 
 class PezDispenser
- 
-# your code here!
- 
+	def initialize(flavors_array)
+		@flavors = flavors_array
+	end
+
+	def see_all_pez
+		@flavors.each { |flavor| puts flavor }
+	end
+
+	def add_pez(flavor)
+ 		@flavors << flavor
+ 	end
+
+ 	def get_pez
+ 		@flavors.pop
+ 	end
+
+ 	def pez_count
+ 		@flavors.size
+ 	end
 end
  
 
 
 # 4. Refactored Solution
+=begin 
 
+not very much that can be broken down here
 
-
-
-
-
+=end
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
 flavors = %w(cherry chocolate cola grape lemon orange peppermint raspberry strawberry).shuffle
@@ -52,4 +89,14 @@ puts "Now you have #{super_mario.pez_count} pez!"
 
 
 
-# 5. Reflection 
+# 5. Reflection
+=begin 
+
+I really felt pretty strong with this one.  After researching for my blog post last week
+on Ruby Classes, I felt like I had overcome some of my insecurities.  It became evident
+to me when I was planning out the layout of my Class structure.  I no longer questioned 
+the relationship with instance variables and simply applied my training to my output. 
+It was really nice to have my first stab at code just work.  I put these methods down
+in less than 10 minutes and it ran right out of the gate.  Super good feeling.
+
+=end
