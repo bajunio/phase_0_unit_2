5f0925691376bf8d35b96e2ae463153540a96fc3
bajunio <bajunio@gmail.com>
2014-05-31 08:29:03 -0700

    Challenge 2, fully complete

--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -44,7 +44,7 @@ end
 end #class
 
 class Silverware
-attr_reader :type
+attr_reader :type, :clean
 
 # Are there any more methods needed in this class? => Yes, clean_silverware
 
@@ -64,6 +64,7 @@ def clean_silverware
 end
 
 end
+
 
 knife1 = Silverware.new("knife")
 
@@ -93,17 +94,39 @@ silverware_drawer.view_contents #What should this return? => Empty drawer!
 fork = Silverware.new("fork")
 silverware_drawer.add_item(fork)
 fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
-
+# Ended up creating a new fork (we dumped it earlier) and added it to the drawer.
 fork.eat
 
-#BONUS SECTION
-puts fork.clean_silverware
+#BONUS SECTION => Added clean to attr_reader so it could be called outside of the class
+puts fork.clean
 
 # DRIVER TESTS GO BELOW THIS LINE
 
+def assert
+  raise "Assertion failed!" unless yield
+end
 
-
-
-
+assert {silverware_drawer.class == Drawer}
+assert {silverware_drawer.contents.empty? == true}
+assert {fork.clean == false}
 
 # 5. Reflection 
+=begin 
+
+I had to restart this challenge.  My first attempt took me down so many
+rabbit holes.  I had setup numerous conditional statements to handle if
+the Silverware were clean or not before it would be allower back into
+the drawer.  This was fine and I felt like I had control over the code, 
+however it felt that I was completely altering the existing driver code.
+I figured it would be best to stick with what was provided and make it
+work before I went out into space.
+
+Overall, the challenge was fairly straight forward.  It seems that I 
+unlocked some understanding that has made working wihin classes a bit
+easier.  I had looked into this challenge at the begining of my week's
+study and found all of the rabbit holes.  It was today that I attacked
+it in a different way and opted to just fix the problems as they were 
+given to me and not try to completely reinvent the code base adding in
+all sorts of restrictions.
+
+=end
